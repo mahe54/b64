bebf6282b7a19789c353b712c8c0d8228a87eaec
Mats M2s Hellström H7m <mats.z.hellstrom@teliacompany.com>
2023-06-08 09:19:16 +0200

    support os.stdin

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"encoding/base64"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,34 +16,39 @@ func main() {
 
 	flag.Parse()
 
+	var lastArg string
 	args := flag.Args()
 	if len(args) > 0 {
-		lastArg := args[len(args)-1]
-
-		if *eFlag {
-			var encodedArg string
-			if *uFlag {
-				encodedArg = base64.RawURLEncoding.EncodeToString([]byte(lastArg))
-			} else {
-				encodedArg = base64.StdEncoding.EncodeToString([]byte(lastArg))
-			}
-			fmt.Println(encodedArg)
-		} else if *dFlag {
-			var decodedArg []byte
-			var err error
-			if *uFlag {
-				decodedArg, err = base64.RawURLEncoding.DecodeString(lastArg)
-			} else {
-				decodedArg, err = base64.StdEncoding.DecodeString(lastArg)
-			}
-			if err != nil {
-				fmt.Println("Error: Invalid base64 string.")
-				os.Exit(1)
-			}
-			fmt.Println(string(decodedArg))
-		}
+		lastArg = args[len(args)-1]
 	} else {
-		fmt.Println("Error: No string provided.")
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		lastArg = strings.TrimSuffix(input, "\n")
+	}
+
+	if *eFlag {
+		var encodedArg string
+		if *uFlag {
+			encodedArg = base64.RawURLEncoding.EncodeToString([]byte(lastArg))
+		} else {
+			encodedArg = base64.StdEncoding.EncodeToString([]byte(lastArg))
+		}
+		fmt.Println(encodedArg)
+	} else if *dFlag {
+		var decodedArg []byte
+		var err error
+		if *uFlag {
+			decodedArg, err = base64.RawURLEncoding.DecodeString(lastArg)
+		} else {
+			decodedArg, err = base64.StdEncoding.DecodeString(lastArg)
+		}
+		if err != nil {
+			fmt.Println("Error: Invalid base64 string.")
+			os.Exit(1)
+		}
+		fmt.Println(string(decodedArg))
+	} else {
+		fmt.Println("Error: No operation specified.")
 		os.Exit(1)
 	}
 }
